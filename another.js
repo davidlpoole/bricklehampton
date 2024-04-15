80529d0b8505916bde0dd85e3b334fd9093436e4
David Poole <14233215+davidlpoole@users.noreply.github.com>
2024-04-15 18:24:04 +1200

    another

---
+++ another.js
@@ -0,0 +1,40 @@
+function generateCombinationsAndCheck(length, targetString) {
+  const characters = ['l', 'r', '!']
+
+  function generateAndCheck(prefix, remainingLength) {
+    if (remainingLength === 0) {
+      if (decodeAndCheck(prefix, targetString)) {
+        console.log(prefix)
+      }
+      return
+    }
+
+    for (let i = 0; i < characters.length; i++) {
+      generateAndCheck(prefix + characters[i], remainingLength - 1)
+    }
+  }
+
+  for (let i = 1; i <= length; i++) {
+    generateAndCheck('', i)
+  }
+}
+
+function decodeAndCheck(combination, targetString) {
+  const run = (script, word) =>
+    script.reduce((word, i) => {
+      switch (i) {
+        case 'l':
+          return word.slice(1) + word[0]
+        case 'r':
+          return word.at(-1) + word.slice(0, -1)
+        case '!':
+          return word[3] + word[2] + word[1] + word[0] + word.slice(4)
+        default:
+          return word
+      }
+    }, word)
+
+  return run([...combination], '') === targetString
+}
+
+generateCombinationsAndCheck(15, 'bricklehampton')
